a78fad612f094f0a6b83c5b2e83dccf789f530a2
atakzimpara39 <88443368+dancingmadman2@users.noreply.github.com>
2024-01-26 16:47:59 +0300

    0.0000000001

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,89 @@
-import 'package:chess_vision/screens/home_screen.dart';
+import 'package:chess_vision/styles.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  static final List<Widget> _screens = <Widget>[
+    const HomeScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarBrightness: Brightness.light,
+          statusBarIconBrightness: Brightness.dark,
+          systemNavigationBarColor: primary,
+          systemNavigationBarIconBrightness: Brightness.dark),
     );
+    return MaterialApp(
+      home: Scaffold(
+
+          //wrap this with Stack and add NoConnection() widget
+          body: _screens.elementAt(selectedIndex),
+          bottomNavigationBar: BottomNavigationBar(
+            iconSize: 30,
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: primary,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.home),
+                label: 'HOME',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.chart_bar_fill),
+                label: 'PUZZLES',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.search),
+                label: 'GAMES',
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.more_horiz), label: 'MORE')
+            ],
+            currentIndex: selectedIndex,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: properGrey,
+            showUnselectedLabels: true,
+            showSelectedLabels: true,
+            unselectedLabelStyle: navUnselected,
+            selectedLabelStyle: navSelected,
+            onTap: _onTappedBar,
+          )),
+    );
+  }
+
+  void _onTappedBar(int value) {
+    setState(() {
+      selectedIndex = value;
+    });
   }
 }
---
+++ lib/styles.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle navSelected = GoogleFonts.bebasNeue(
+    fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold);
+TextStyle navUnselected = GoogleFonts.bebasNeue(
+  fontSize: 13,
+);
+
+Color lightGrey = const Color(0xFFe5e5e5);
+Color lightBlack = const Color(0xFF121212);
+Color darkGrey = const Color(0xFF757575);
+Color properRed = const Color(0xFFdc1a22);
+Color properGrey = const Color(0xFF96969E);
+Color properWhite = const Color(0xFFFAFAFA);
+Color skeleton = const Color(0xFFEBEBF4);
+
+Color background = const Color(0xFFF4F6F5);
+Color primary = const Color(0xFF433d48);
+Color secondary = const Color(0xFFcdd5d2);
+Color accent = const Color(0xFF746d63);
+Color textBlack = const Color(0xFF070808);
